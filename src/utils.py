397651da08aab171d3bfa1a4eb71b78b16a5cf33
Joao <joaopedrodesanchespinheiro2@gmail.com>
2026-04-12 16:14:59 -0300

    feat: add specifications for compounds such as proteins, polyphenols, etc which were missing before

--- src/utils.py
+++ src/utils.py
@@ -57,7 +57,7 @@ def extract_wine_metrics(serie_filtrada):
     # Região Açúcar: 900 - 1150
     # Região Aroma (Ésteres): 1730 - 1750
     
-    # Exemplo para Aroma:
+    # Aroma (Ésteres):
     mask_aroma = (serie_filtrada.index >= 1730) & (serie_filtrada.index <= 1750)
     segmento_aroma = serie_filtrada[mask_aroma]
     if not segmento_aroma.empty:
@@ -65,11 +65,32 @@ def extract_wine_metrics(serie_filtrada):
         # Regra do Trapézio para Integral (Concentração)
         metrics['area_aroma'] = np.trapz(segmento_aroma.values, x=segmento_aroma.index)
         
-    # Exemplo para Açúcar:
-    mask_açúcar = (serie_filtrada.index >= 900) & (serie_filtrada.index <= 1150)
-    segmento_açúcar = serie_filtrada[mask_açúcar]
-    if not segmento_açúcar.empty:
-        metrics['pico_açúcar'] = segmento_açúcar.max()
-        metrics['area_açúcar'] = np.trapz(segmento_açúcar.values, x=segmento_açúcar.index)
+    # Açúcar:
+    mask_acucar = (serie_filtrada.index >= 900) & (serie_filtrada.index <= 1150)
+    segmento_acucar = serie_filtrada[mask_acucar]
+    if not segmento_acucar.empty:
+        metrics['pico_acucar'] = segmento_acucar.max()
+        metrics['area_acucar'] = np.trapz(segmento_acucar.values, x=segmento_acucar.index)
+    
+    # Polifenois:
+    mask_polifenois = (serie_filtrada.index >= 1400) & (serie_filtrada.index <= 1600)
+    segmento_polifenois = serie_filtrada[mask_polifenois]
+    if not segmento_polifenois.empty:
+        metrics['pico_polifenois'] = segmento_polifenois.max()
+        metrics['area_polifenois'] = np.trapz(segmento_polifenois.values, x=segmento_polifenois.index)
+    
+    # Proteínas:
+    mask_proteinas = (serie_filtrada.index >= 1600) & (serie_filtrada.index <= 1700)
+    segmento_proteinas = serie_filtrada[mask_proteinas]
+    if not segmento_proteinas.empty:
+        metrics['pico_proteinas'] = segmento_proteinas.max()
+        metrics['area_proteinas'] = np.trapz(segmento_proteinas.values, x=segmento_proteinas.index)
+    
+    # Ácidos Organicos (1200 cm-1): 
+    mask_acidos = (serie_filtrada.index >= 1160) & (serie_filtrada.index <= 1240)
+    segmento_acidos = serie_filtrada[mask_acidos]
+    if not segmento_polifenois.empty:
+        metrics['pico_acidos'] = segmento_acidos.max()
+        metrics['area_acidos'] = np.trapz(segmento_acidos.values, x=segmento_acidos.index)
 
     return metrics
